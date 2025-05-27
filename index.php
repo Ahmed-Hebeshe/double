37738f11a642b864107ce3433ab9fab982de76e8
Ahmed-Hebeshe <ahmedhebeshe@gmail.com>
2025-05-27 15:12:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,2 @@
 <?php
-if (isset($_GET['url'])) {
-    $url = $_GET['url'];
-    $content = file_get_contents($url);
-    echo $content;
-} else {
-    echo '<form method="get">
-        <input name="url" placeholder="Enter URL to proxy" style="width:300px;">
-        <button type="submit">Go</button>
-    </form>';
-}
+echo "Hello from proxy test!";
